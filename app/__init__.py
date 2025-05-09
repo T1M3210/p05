12bb241dbb4a288688e52200f19082825b30c2b1
Tim <timngstuy@gmail.com>
2025-05-09 06:13:34 -0400

    updated login/sign up, requirements.txt, etc

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, url_for, flash
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def home():
+    return render_template('home.html')
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.find_one({"username": username}):
+            flash("Username already exists.")
+            return redirect(url_for("register"))
+
+        hashed_pw = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        users.insert_one({"username": username, "password": hashed_pw})
+        flash("Account created! Please log in.")
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = users.find_one({"username": username})
+        if user and bcrypt.checkpw(password.encode("utf-8"), user["password"]):
+            session["user"] = username
+            return redirect(url_for("home"))
+        else:
+            flash("Invalid login credentials.")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    flash("Logged out.")
+    return redirect(url_for("login"))
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
