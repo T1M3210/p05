e612fd6272f7cf3544529eea714e3cbe0872713f
Stella Yampolsky <stellagraceyampolksky@gmail.com>
2025-05-14 10:53:05 -0400

    rules

--- app/rules/rules.js
+++ app/rules/rules.js
@@ -1,2 +1,10 @@
-//write simple rules ex "password must have x"
+import Rule from "./Rule";
 
+const rules = [
+    new Rule(
+        "Your password must have a number",
+        (input) => /\d/.test(input)
+    ),
+];
+
+export default rules;
